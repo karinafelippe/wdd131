1417b23d851e2e7874cd00f6d30e976e5dff7b72
karinafelippe <kaninasud@gmail.com>
2025-03-04 22:10:32 -0300

    W01 Assignment: Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,13 @@
+/* 
+  Author: Karina Felippe
+*/
+
+// select the DOM elements for output
+const year = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+// use the date object
+const today = new Date();
+
+year.innerHTML = today.getFullYear();
+lastModified.innerHTML = `Last Modification: ${document.lastModified}`;
