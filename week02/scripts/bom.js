4403f5a1d8269962bfb0793c1c7e97047478f990
karinafelippe <kaninasud@gmail.com>
2025-03-10 18:21:24 -0300

    w2 learning activities - hamburguer menu

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,10 +1,22 @@
 const input = document.querySelector('#favchap');
 const btnAdd = document.querySelector('button');
 const list = document.querySelector('#list');
-const li = document.createElement('li');
-const btnDelete = document.createElement('button');
 
-li.textContent = input.value;
-btnDelete.textContent = '❌';
-li.append(btnDelete);
-list.append(li);
+btnAdd.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const btnDelete = document.createElement('button');
+        li.textContent = input.value;
+        btnDelete.textContent = '❌';
+        li.append(btnDelete);
+        list.append(li);
+        input.value = '';
+        input.focus();
+
+        btnDelete.addEventListener('click', function () {
+            list.removeChild(li);
+            input.value = '';
+            input.focus();
+        });
+    }
+});
